353140ce6555c93c6f688a49313477b7eb584eb5
jrkingsly <j11622@gmail.com>
2018-01-06 16:37:06 -0800

    Init Commit

---
+++ assets/js/scripts/custom.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function(){
+	jQuery('#hamburger').click(function(){
+		jQuery(this).toggleClass('open');
+	});
+});
---
+++ page-templates/home.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Template Name: Homepage
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="intro">
+	<div class="row">
+		<div class="small-12 columns">
+			<h1>Hello</h1>
+			<div class="row">
+				<div class="small-12 medium-3 columns">
+					<div class="hp-selfie parallax-window" data-parallax="scroll" data-image-src="/jrkingsly/wp-content/themes/jrkingsly/assets/images/me_2.jpg" style="border-radius: 50%;"></div>	
+				</div>
+				<div class="small-12 medium-9 columns">	
+					<p>Welcome to my site. My name is Justin Kingsly Rosen and I'm a WordPress developer. I live in Las Vegas and work for R&R Partners.</p>
+					<p>This site is built in WordPress using a custom theme. No templates here. I'm using Foundation 6 as my framework with SCSS. I'm using a GitHub repo and Gulp to commit my changes from local to live.</p>
+				</div>	 
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="small-12 medium-8 columns">	
+		<h1>A little about me</h1>		
+		<p>I started my career as a Digital Project Manager for R&R. After realizing that </p>
+		sdkjhgskldgsdgnskd
+	</div>		
+	<div class="small-12 medium-4 columns">
+		<h1>Skills</h1>
+		<h3>Languages</h3>
+		<ul>
+			<li>HTML</li>
+			<li>CSS/SCSS</li>
+			<li>JS/JQuery</li>
+			<li>PHP</li>
+		</ul>
+		<h3>Frameworks</h3>
+		<ul>
+			<li>Zurb Foundation (preferred)</li>
+			<li>Bootstrap</li>
+		</ul>
+		<h3>CMS/Software</h3>
+		<ul>
+			<li>WordPress</li>
+			<li>Photoshop</li>
+			<li>Google Web Designer</li>
+		</ul>			
+	</div>	
+</div>		
+
+<?php get_footer(); ?>
