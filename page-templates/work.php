d706d7ea23df7ebce60bd254ed2536a6d13b144d
jrkingsly <j11622@gmail.com>
2018-02-12 11:43:12 -0800

    Work page updates

--- page-templates/work.php
+++ page-templates/work.php
@@ -6,10 +6,61 @@ Template Name: Work
 
 <?php get_header(); ?>
 
-<div class="row">
+<div class="work row">
 	<div class="small-12 columns">
-		<h1>Under Construction. Check back soon.</h1>
+		<h1>Work</h1>
+		<p>This page is a work-in-progress. The layout isn't flashy (<span style="color: #FF7200;">yet</span>), but in the mean time, below is a list of clients and websites that I have worked on.</p>
+		<p style="color: #FF7200;">Please note that the links below may have changed since my involvement.</p>
+	</div>
+	<div class="small-12 columns">
+		<h3>Las Vegas Convention and Visitors Authority (LVCVA)</h3>
+			<p><a href="https://www.visitlasvegas.com/" target="_blank">Visit Las Vegas (website)</a></p>
+			<p><a href="https://www.vegasmeansbusiness.com/" target="_blank">Vegas Means Business (website)</a></p>
+			<p><a href="http://www.lvcva.com/" target="_blank">LVCVA (website)</a></p>
+			<p><a href="https://www.visitlaughlin.com/" target="_blank">Visit Laughlin (website)</a></p>
+			<p><a href="https://www.visitmesquite.com/" target="_blank">Visit Mesquite (website)</a></p>
+			<p><a href="http://khaledaward.rrpartnersdev.com/" target="_blank">DJ Khaled Award Submission (landing page)</a></p>
+		</ul>
+	</div>
+	<div class="small-12 columns">	
+		<h3>Small Town Brewery</h3>
+		<p><a href="http://smalltownbrewery.com/" target="_blank">Website</a></p>
+	</div>
+	<div class="small-12 columns">
+		<h3>Utah Department of Health Division</h3>
+		<p><a href="http://waytoquit.org/" target="_blank">Way to Quit (website)</a>
+	</div>
+	<div class="small-12 columns">
+		<h3>Pharmaceutical Public Awareness Campaign (Utah)</h3>
+		<p><a href="http://useonlyasdirected.org/" target="_blank">Use Only As Directed (website)</a></p>
+	</div>
+	<div class="small-12 columns">
+		<h3>Days of '47 Rodeo Salt Lake City</h3>
+		<p><a href="http://daysof47cowboygames.com/" target="_blank">Days of '47 Cowboy Games (website)</a></p>
+	</div>
+		<div class="small-12 columns">
+		<h3>Utah Dept. of Alcohol Beverage Control</h3>
+		<p><a href="http://parentsempowered.org/" target="_blank">Parents Empowered (website)</a></p>
+	</div>
+	<div class="small-12 columns">
+		<h3>#VegasStrong Resiliency Center</h3>
+		<p><a href="https://vegasstrongrc.org/" target="_blank">#VegasStrong Resiliency Center for Victims of 1 October (website)</a></p>
+	</div>
+	<div class="small-12 columns">
+		<h3>Staker Parson</h3>
+		<p><a href="http://stakerparson.com/" target="_blank">Staker Parson (multi-site installation)</a></p>
+	</div>
+	<div class="small-12 columns">
+		<h3>R&R Partners Foundation</h3>
+		<p><a href="http://rrpartnersfoundation.org/" target="_blank">R&R Partners Foundation (website)</a></p>
+		<p><a href="http://www.projectelcrucero.org/" target="_blank">Project El Crucero (website)</a></p>
+		<p><a href="http://flipthescriptnow.org/" target="_blank">Flip the Script Anti-Bullying (website)</a></p>
 	</div>	
+	<div class="small-12 columns">
+		<h3>MSU Las Vegas Spartans Alumni Group</h3>
+		<p><a href="http://www.lasvegasspartans.com/" target="_blank">Website</a></p>
+	</div>
+						
 </div>
 
 <?php get_footer(); ?>	
